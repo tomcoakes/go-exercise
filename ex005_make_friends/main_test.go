411d5552759b813a28fe8f83b263283a4bf4b92f
Tom Coakes <tomcoakes@gmail.com>
2016-10-18 16:13:19 +0100

    add another test case to ex005

--- ex005_make_friends/main_test.go
+++ ex005_make_friends/main_test.go
@@ -3,16 +3,23 @@ package main
 import "testing"
 
 var listFriendsTests = []struct {
-	p1, p2 person
-	out    string
+	p1, p2, p3 person
+	out        string
 }{
-	{person{name: "James"}, person{name: "Alex"}, "James's friends are Alex"},
-	{person{name: "Peter"}, person{name: "Phillip"}, "Peter's friends are Phillip"},
+	{p1: person{name: "James"}, p2: person{name: "Alex"}, out: "James's friends are Alex"},
+	{p1: person{name: "Peter"}, p2: person{name: "Phillip"}, out: "Peter's friends are Phillip"},
+	{p1: person{name: "Donald"}, p2: person{name: "Hillary"}, p3: person{name: "Melania"}, out: "Donald's friends are Hillary, Melania"},
 }
 
 func TestFriendMaker(t *testing.T) {
 	for _, test := range listFriendsTests {
 		test.p1, test.p2 = makeFriends(test.p1, test.p2)
+
+		// for testing the example in the test table with a third person
+		if test.p3.name != "" {
+			test.p1, test.p3 = makeFriends(test.p1, test.p3)
+		}
+
 		if listFriends(test.p1) != test.out {
 			t.Errorf("Expected %s but received %s", listFriends(test.p1), test.out)
 		}
